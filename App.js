aba8e30f0ce7e4492bfef7d985cb2cf45bebf56e
Ben S. Anderson <ben.anderson@assurant.com>
2019-03-06 08:51:04 -0500

    updated app to use sensor data

--- App.js
+++ App.js
@@ -1,50 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Text } from 'react-native';
+import { barometer } from 'react-native-sensors';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class AddItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      severity: 2,
+      subscription: null,
+      pressure: 0.0
+    };
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
+  componentDidMount() {
+    const subscription = barometer.subscribe(({pressure}) => {
+      console.log(`Pressure is ${pressure}`);
+      this.setState({pressure});
+    });
+    this.setState({subscription});
+  }
+
+  componentWillUnmount() {
+
+  }
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+    return (<Text>Current Pressure: {this.state.pressure.toFixed(2)}</Text>);
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
